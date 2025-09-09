640ef03eb01fe5ef58bc21761beab07a4a5aea6c
bhatahrar <bhatahrar222@gmail.com>
2025-09-09 10:17:00 +0000

    add users table and users model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,22 @@
 class ArticlesController < ApplicationController
+    before_action :set_article,only: [:show,:edit,:update,:destroy]
     def show 
         # byebug
-        @article=Article.find(params[:id])
     end
     def index
         @articles=Article.all
     end 
     def new
         @article=Article.new
-    end
+    end 
 
     def create
         # byebug  
-        @article=Article.new(params.require(:article).permit(:title,:description))
+        @article=Article.new(article_params)
         # render plain:@article.inspect
             if @article.save
                 flash[:notice]="Article was created successfully.."
-        redirect_to article_path(@article)
+        redirect_to articles_path
         else 
             flash[:alert]="Article was not created.."
             render :new, status: :unprocessable_entity
@@ -24,15 +24,25 @@ class ArticlesController < ApplicationController
     end
     def edit
       
-        @article=Article.find(params[:id])
     end
     def update
-        @article=Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title,:description))
+        if @article.update(article_params)
             flash[:notice]="Article has been successfully updated"
             redirect_to @article
         else 
             render :edit, status: :unprocessable_entity
         end
+    end
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+    private
+    def set_article
+        @article=Article.find(params[:id])
+    end
+    private
+    def article_params
+        params.require(:article).permit(:title,:description)
     end
 end
