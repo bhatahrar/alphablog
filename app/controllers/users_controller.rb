c511465e813878f5965c967a3fff9969ed3647ac
bhatahrar <bhatahrar222@gmail.com>
2025-09-10 12:13:33 +0000

    users create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+        def new
+        @user=User.new
+        end
+        def create 
+                # byebug
+                @user=User.new(user_params)
+                if @user.save
+                        flash[:notice]="Welcome to the blog #{@user.username}, you have sucessfully signed up"
+                        redirect_to articles_path
+                else
+                        render :new,status: :unprocessable_entity
+                end
+        end
+
+        private
+        def user_params
+                params.require(:user).permit(:username,:email,:password)
+        end
+end
